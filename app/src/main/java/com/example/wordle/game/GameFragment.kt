facbcd22bdeb5651b1949dcb6623a9b379f83e5e
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-04-18 00:22:15 -0600

    - Added viewBinding like I was supposed to instead of dataBinding - Beginning to setup the GameFragment class up - Added ~5k words to the project. Not by hand though but it took an embarrassingly long amount of time to do this but it is done and the app doesn't crash

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -1,13 +1,27 @@
 package com.example.wordle.game
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.wordle.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.wordle.databinding.FragmentGameBinding
 
-class GameFragment : Fragment(R.layout.fragment_game) {
+class GameFragment : Fragment() {
+    private val viewModel: GameViewModel by viewModels()
+    private lateinit var binding: FragmentGameBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
 }
