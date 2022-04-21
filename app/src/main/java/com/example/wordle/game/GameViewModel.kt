23ffe5a52398ac301a56b6da3771b342b57adf7a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-04-21 17:01:26 -0600

    ViewModel Changes - Created a bunch of TODO comments for how my viewModel should work when I develop it. I got finals to study for! Can't do it right now!!

--- app/src/main/java/com/example/wordle/game/GameViewModel.kt
+++ app/src/main/java/com/example/wordle/game/GameViewModel.kt
@@ -2,7 +2,20 @@ package com.example.wordle.game
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+const val NUMBER_OF_GUESSES = 6
+
+
 
 class GameViewModel(): ViewModel(){
+    //TODO: Set up a variable that knows the random word and how many tries the user has
 
+    //TODO: Set up a variable that generates a random word
+
+    //TODO: Function that determines which editTexts are open for editing
+
+    //TODO: Setup Text Validation functions (Letters ONLY)
+
+    //TODO: Setup functions for deciding which letters are green, yellow and grey
+
+    //TODO: Setup
 }
