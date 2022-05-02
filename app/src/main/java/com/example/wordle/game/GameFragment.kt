6e3f6a53a82bf3da85b73de6779e36d8f56a3f01
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-02 11:03:30 -0600

    The keyboard shows up when a square is pressed

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -3,10 +3,7 @@ package com.example.wordle.game
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewAnimationUtils
-import android.view.ViewGroup
+import android.view.*
 import android.view.inputmethod.InputMethodManager
 import androidx.core.view.forEach
 import androidx.fragment.app.Fragment
@@ -29,19 +26,24 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.letterGrid.forEach {
-            Log.i("letter!", "word id: ${resources.getResourceName(it.id)}")
+            //Log.i("letter!", "word id: ${resources.getResourceName(it.id)}")
             if (it is ViewGroup){
                 for (i in 0 until it.childCount) {
                     val child = it.getChildAt(i)
-                    Log.i("letter!", "word ${i+1} letter id: ${resources.getResourceName(child.id)}")
+                    child.setOnClickListener{
+                        child.isFocusableInTouchMode = true //This will create bugs in the future btw
+                        showKeyboard(child,requireContext())
+                    }
+                    //Log.i("letter!", "word ${i+1} letter id: ${resources.getResourceName(child.id)}")
                 }
             }
         }
-
     }
 }
 
     fun showKeyboard(view: View, context: Context) {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        if (view.requestFocus()) {
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
