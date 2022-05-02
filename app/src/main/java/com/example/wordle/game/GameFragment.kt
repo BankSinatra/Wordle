2ebbfd0d584cf1bcd22ef5ee2ab30523d58f266e
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-01 18:34:35 -0600

    Accessing each item in the grid via loops - Took and embarrassing amount of time

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -1,9 +1,14 @@
 package com.example.wordle.game
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewAnimationUtils
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.view.forEach
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.wordle.databinding.FragmentGameBinding
@@ -11,6 +16,7 @@ import com.example.wordle.databinding.FragmentGameBinding
 class GameFragment : Fragment() {
     private val viewModel: GameViewModel by viewModels()
     private lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +28,20 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.letterGrid.forEach {
+            Log.i("letter!", "word id: ${resources.getResourceName(it.id)}")
+            if (it is ViewGroup){
+                for (i in 0 until it.childCount) {
+                    val child = it.getChildAt(i)
+                    Log.i("letter!", "word ${i+1} letter id: ${resources.getResourceName(child.id)}")
+                }
+            }
+        }
 
     }
 }
+
+    fun showKeyboard(view: View, context: Context) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+    }
