9fd5f30ac64a6f3dc2156e8514db06650f9b639b
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-03 00:29:08 -0600

    ViewModel Changes - Realized we don't really need LiveData. Removed that and now I'm using regular strings and ints - Setting up a model for tracking which editText should be editable.

--- app/src/main/java/com/example/wordle/game/GameViewModel.kt
+++ app/src/main/java/com/example/wordle/game/GameViewModel.kt
@@ -1,32 +1,53 @@
 package com.example.wordle.game
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.wordle.models.LetterBlock
+
 const val NUMBER_OF_GUESSES = 6
 
 
+class GameViewModel() : ViewModel() {
+    private var _currentLetter: Int = 0
+    private var _currentWord: Int = 0
+    val currentLetter = _currentLetter
+    val currentWord = _currentWord
+    lateinit var word: String
+    lateinit var grid: List<List<LetterBlock>>
 
-class GameViewModel(): ViewModel(){
-    private val _currentWord = MutableLiveData<String>()
-    val currentWord : LiveData<String> = _currentWord
-
-    private val _userGuessCount = MutableLiveData<Int>(0)
-    val userGuessCount : LiveData<Int> = _userGuessCount
-
-    private fun generateWord(){
-        val word = allWordsList.random()
-    }
-
-    private fun resetGame(){
+    init {
+        resetGame()
         generateWord()
-        _userGuessCount.value = 0
+        grid = generateGrid()
     }
 
-    private fun textValidation(char: Char): Boolean{
+    private fun generateGrid(): List<List<LetterBlock>> {
+        val myList = mutableListOf(mutableListOf<LetterBlock>())
+        for (word in 0..5) {
+            myList.add(mutableListOf())
+            for (letter in 0..4)
+                myList[word].add(LetterBlock())
+        }
+        return myList.toList()
+    }
+
+    fun findFocus() {
+        //TODO: This sets the focus for the right editText
+    }
+
+    private fun generateWord() {
+        word = allWordsList.random()
+    }
+
+    private fun resetGame() {
+        generateWord()
+        _currentLetter = 0
+        _currentWord = 0
+    }
+
+    private fun textValidation(char: Char): Boolean {
         val pattern = Regex("[a-zA-Z]") //If the character input is a letter
         return pattern.containsMatchIn(char.toString())
     }
 
-    //TODO: Setup functions for deciding which letters are green, yellow and grey
+
 }
