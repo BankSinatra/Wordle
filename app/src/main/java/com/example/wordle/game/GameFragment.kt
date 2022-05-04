fdb41896f46d18de2365ecb9e6f3eae31775afdf
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-03 18:26:38 -0600

    Changes to the way the game looks and functions - The keyboard opens and edits stuff when pressed - It'll open the right keyboard depending on which guess they're on

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.forEach
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
@@ -31,23 +32,33 @@ class GameFragment : Fragment() {
         binding.letterGrid.forEach {
             if (it is ViewGroup) {
                 for (i in 0 until it.childCount) {
-                    //TODO: Do something here
+                    val child = it.getChildAt(i)
+                    child.setOnClickListener {
+                        showKeyBoard(binding.guess1, requireContext())
+                    }
                 }
             }
         }
-        val m = findChildAtIndex(binding.letterGrid[viewModel.currentLetter], viewModel.currentWord)
-        Log.i("Word", resources.getResourceName(m.id))
-
-
+    }
+    fun findEditTextByWord(): View{
+        val x = binding.guess1
+        when (viewModel.currentWord){
+            1 -> return binding.guess2
+            2 -> return binding.guess3
+            3 -> return binding.guess4
+            4 -> return binding.guess5
+            5 -> return binding.guess6
+        }
+        return x
     }
 }
 
 
-fun findChildAtIndex(view: View, index: Int) : View{
-    var myView = view
-    if (view is ViewGroup) {
-        myView = view[index]
 
+fun showKeyBoard(view: View, context: Context ){
+    view.requestFocus()
+    if (view.requestFocus()) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
     }
-    return myView
 }
