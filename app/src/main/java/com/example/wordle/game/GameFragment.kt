d313cd6d59c9cc98d75cb692fac2a5a6a513ceb6
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-05 16:50:26 -0600

    Got the keyboard to work!

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -1,5 +1,6 @@
 package com.example.wordle.game
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -7,9 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
+import android.widget.EditText
 import androidx.core.view.forEach
-import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.wordle.databinding.FragmentGameBinding
@@ -29,34 +29,36 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.letterGrid.forEach {
-            if (it is ViewGroup) {
-                for (i in 0 until it.childCount) {
-                    val child = it.getChildAt(i)
+        //showKeyBoard(findEditTextByWord(), requireContext())
+        binding.letterGrid.forEach { view ->
+            if (view is ViewGroup) {
+                for (i in 0 until view.childCount) {
+                    val child = view.getChildAt(i)
                     child.setOnClickListener {
-                        showKeyBoard(findEditTextByWord(), requireContext())
+                        val ed = findEditTextByWord()
+                        ed.requestFocus()
+                        showKeyBoard(ed, requireContext())
                     }
                 }
             }
         }
     }
-    private fun findEditTextByWord(): View{
-        val x = binding.guess1
-        when (viewModel.currentWord){
-            1 -> return binding.guess2
-            2 -> return binding.guess3
-            3 -> return binding.guess4
-            4 -> return binding.guess5
-            5 -> return binding.guess6
+    private fun findEditTextByWord(): EditText {
+        return when (viewModel.currentWord){
+            0 -> binding.guess1
+            1 -> binding.guess2
+            2 -> binding.guess3
+            3 -> binding.guess4
+            4 -> binding.guess5
+            else -> binding.guess6
         }
-        return x
     }
 }
 
-fun showKeyBoard(view: View, context: Context ){
-    view.requestFocus()
-    if (view.requestFocus()) {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
-    }
+fun showKeyBoard(editText: EditText, context: Context){
+    editText.requestFocus()
+
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+
 }
