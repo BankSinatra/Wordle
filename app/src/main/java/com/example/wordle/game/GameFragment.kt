4a01527b4ba17de8cbed29e14f94081cd17bb237
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-06 20:18:59 -0600

    Beginning an architecture overhaul - Changing the text entry process

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -7,10 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.core.view.forEach
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.wordle.databinding.FragmentGameBinding
@@ -30,61 +27,36 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //showKeyBoard(findEditTextByWord(), requireContext())
+        setClickListeners()
+    }
+
+    private fun showKeyBoard(editText: EditText){
+        editText.requestFocus()
+        val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+
+    }
+
+    private fun setClickListeners(){
         binding.letterGrid.forEach {
             if (it is ViewGroup) {
+                //For each item in the viewGroup (Each row)...
                 for (i in 0 until it.childCount) {
+                    //..Set a click listener for the item
                     val child = it.getChildAt(i)
                     child.setOnClickListener {
-                        showKeyBoard(findEditTextByWord(), requireContext())
+                        showKeyBoard(binding.guessField)
                     }
                 }
             }
         }
-        findEditTextByWord().doOnTextChanged { text, start, before, count ->
-            val linearLayout = findLinearLayoutByWord()
-            linearLayout.forEach {
-                if (it is TextView){
-                    it.text =""
-                }
-                if (findEditTextByWord().text.isNotEmpty()){
-                    for (x in findEditTextByWord().text.indices){
-                        val child =  linearLayout.getChildAt(x)
-                        if (child is TextView){
-                            child.text = findEditTextByWord().text[x].toString().uppercase()
-                        }
-                    }
-                }
-            }
-
-        }
-    }
-    private fun findEditTextByWord(): EditText {
-        return when (viewModel.currentWord){
-            0 -> binding.guess1
-            1 -> binding.guess2
-            2 -> binding.guess3
-            3 -> binding.guess4
-            4 -> binding.guess5
-            else -> binding.guess6
-        }
     }
 
-    private fun findLinearLayoutByWord() : LinearLayout{
-        return when (viewModel.currentWord) {
-            0 -> binding.word1
-            1 -> binding.word2
-            2 -> binding.word3
-            3 -> binding.word4
-            4 -> binding.word5
-            else -> binding.word6
+    //This makes certain square rows un-Clickable
+    private fun removeClickListeners(wordRow: ViewGroup){
+        wordRow.forEach {
+            it.isClickable = false
         }
     }
 }
 
-fun showKeyBoard(editText: EditText, context: Context){
-    editText.requestFocus()
-    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
-
-}
