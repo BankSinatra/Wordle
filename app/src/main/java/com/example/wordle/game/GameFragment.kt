c7ca50df6a7859425e14c38137e43d7c5e3e4503
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-07 16:17:39 -0600

    Changes to the GameFragment - Changed the keyboardSetUp() function to check if the word is a real word - Added code that translates the color map to actual colors

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -12,11 +12,13 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.forEach
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.wordle.R
 import com.example.wordle.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -94,10 +96,17 @@ class GameFragment : Fragment() {
                 EditorInfo.IME_ACTION_DONE -> {
                     //Check to see if the string length is 5 then submit it.
                     if (viewModel.guess.value?.length == 5) {
-                        removeClickListeners(wordView) // You can't interact with submitted blocks after submitting
-                        viewModel.submitWord()
+                        if(viewModel.wordValidation(viewModel.guess.value!!)){
+                            changeBackground(wordView)
+                            removeClickListeners(wordView) // You can't interact with submitted blocks after submitting
+                            viewModel.submitWord()
+                        }else{
+                            //TODO: Let the user know that this is not a real word
+                            Toast.makeText(context, "Not in word list", Toast.LENGTH_SHORT).show()
+                        }
                     } else {
                         //TODO: Let the user know that the word is not long enough
+                        Toast.makeText(context, "Not enough letters", Toast.LENGTH_SHORT).show()
                     }
                     true
                 }
@@ -111,12 +120,14 @@ class GameFragment : Fragment() {
             wordView.forEach {
                 if (it is TextView) {
                     it.text = ""
+                    it.setBackgroundResource(R.drawable.letter_block_unsubmitted)
                 }
                 if (binding.guessField.text.isNotEmpty()) { //I should be using the viewModel liveData actually but whatevs
                     for (letterIndex in binding.guessField.text.indices) {
                         val child = wordView.getChildAt(letterIndex)
                         if (child is TextView) {
                             child.text = binding.guessField.text[letterIndex].toString().uppercase()
+                            child.setBackgroundResource(R.drawable.letter_block_unsubmitted_input)
                         }
                     }
                 }
@@ -139,6 +150,20 @@ class GameFragment : Fragment() {
             3 -> binding.word4
             4 -> binding.word5
             else -> binding.word6
+        }
+    }
+
+    private fun changeBackground(view: LinearLayout){
+        val colorMap = viewModel.guess.value?.let { viewModel.evaluateWord(it) }
+        for (letterIndex in 0 until view.childCount) {
+            val child = wordView.getChildAt(letterIndex)
+            if (colorMap?.get(letterIndex) == LetterState.GREEN) {
+                child.setBackgroundResource(R.drawable.letter_block_green)
+            }else if(colorMap?.get(letterIndex) == LetterState.YELLOW){
+                child.setBackgroundResource(R.drawable.letter_block_yellow)
+            }else{
+                child.setBackgroundResource(R.drawable.letter_block_wrong)
+            }
         }
     }
 }
