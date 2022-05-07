1a6233667fd445bc85722763fb91bf7a0cf5ec51
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-05-07 12:56:31 -0600

    GameFragment changes - Added a viewModel observer that changes the row to be edited based on the guess number - Created a filter that only accepts alphabetical words. No symbols or numbers - Added a function that submits the word for evaluation once the go button on the keyboard is pressed - Added the a function to find the linear layout by number

--- app/src/main/java/com/example/wordle/game/GameFragment.kt
+++ app/src/main/java/com/example/wordle/game/GameFragment.kt
@@ -2,19 +2,27 @@ package com.example.wordle.game
 
 import android.content.Context
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.view.forEach
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.wordle.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
-    private val viewModel: GameViewModel by viewModels()
     private lateinit var binding: FragmentGameBinding
+    private lateinit var viewModel: GameViewModel
+    private lateinit var wordView: LinearLayout // Keeps track of the linearlayout that is edited
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,22 +30,32 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentGameBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val wordObserver = Observer<Int> { newNumber ->
+            wordView = findLinearLayoutByNumber(newNumber)
+        }
+        viewModel.guessNumber.observe(viewLifecycleOwner, wordObserver)
+
         setClickListeners()
+        keyboardSetUp()
     }
 
-    private fun showKeyBoard(editText: EditText){
+
+    private fun showKeyBoard(editText: EditText) {
         editText.requestFocus()
         val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
-
     }
 
-    private fun setClickListeners(){
+    private fun setClickListeners() {
         binding.letterGrid.forEach {
             if (it is ViewGroup) {
                 //For each item in the viewGroup (Each row)...
@@ -52,11 +70,77 @@ class GameFragment : Fragment() {
         }
     }
 
+    //Filter for words. Alphabet ONLY
+    class AlphabetFilter : InputFilter {
+        override fun filter(
+            source: CharSequence?, start: Int, end: Int, dest: Spanned?, dstart: Int, dend: Int
+        ): CharSequence {
+            val pattern = Regex("^[a-zA-Z]+\$")
+            if (source == null || source.isBlank()) {
+                return ""
+            }
+            return if (source.isNotEmpty() && source.matches(pattern)) {
+                source
+            } else {
+                ""
+            }
+        }
+    }
+
+    private fun keyboardSetUp() {
+        //When you press the submit button on the keyboard...
+        binding.guessField.setOnEditorActionListener { _, actionId, _ ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    //Check to see if the string length is 5 then submit it.
+                    if (viewModel.guess.value?.length == 5) {
+                        removeClickListeners(wordView) // You can't interact with submitted blocks after submitting
+                        viewModel.submitWord()
+                    } else {
+                        //TODO: Let the user know that the word is not long enough
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+        val letterFilter = AlphabetFilter()
+        val maxLength = InputFilter.LengthFilter(5)
+        binding.guessField.filters = arrayOf(letterFilter, maxLength)
+        binding.guessField.doOnTextChanged { text, start, before, count ->
+            wordView.forEach {
+                if (it is TextView) {
+                    it.text = ""
+                }
+                if (binding.guessField.text.isNotEmpty()) { //I should be using the viewModel liveData actually but whatevs
+                    for (letterIndex in binding.guessField.text.indices) {
+                        val child = wordView.getChildAt(letterIndex)
+                        if (child is TextView) {
+                            child.text = binding.guessField.text[letterIndex].toString().uppercase()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     //This makes certain square rows un-Clickable
-    private fun removeClickListeners(wordRow: ViewGroup){
+    private fun removeClickListeners(wordRow: ViewGroup) {
         wordRow.forEach {
             it.isClickable = false
         }
     }
+
+    private fun findLinearLayoutByNumber(number: Int): LinearLayout {
+        return when (number) {
+            0 -> binding.word1
+            1 -> binding.word2
+            2 -> binding.word3
+            3 -> binding.word4
+            4 -> binding.word5
+            else -> binding.word6
+        }
+    }
 }
+
 
